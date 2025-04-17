ae42e71f8f003936be88b2495ee09c81aacf60cf
OXKIMYXO <dexmareborn0101@gmail.com>
2025-04-17 11:55:59 +0200

    First-commit

---
+++ README.md
@@ -0,0 +1,10 @@
+**Titre** : Système d'Authentification MERN : Inscription et Connexion Sécurisée
+
+**Description :**
+
+Ce projet utilise la stack MERN (MongoDB, Express.js, React, Node.js) pour construire un système d'authentification sécurisé, offrant aux utilisateurs la possibilité de s'inscrire, se connecter et accéder à un site web. Les fonctionnalités incluent :
+
+- Création de comptes avec hachage sécurisé des mots de passe.
+- Connexion et gestion des sessions via JSON Web Token (JWT).
+- Déconnexion et protection des routes pour garantir l'accès uniquement aux utilisateurs authentifiés.
+- Formulaires dynamiques avec validation et retour visuel des erreurs.
---
+++ client/src/context/AppContextProvider.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { AppContent } from "./AppContext";
+import { toast } from "react-toastify";
+import axios from "axios";
+
+export const AppContextProvider = ({ children }) => {
+
+    axios.defaults.withCredentials = true
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [userData, setUserData] = useState({});
+
+
+  const getAuthState = async () => {
+    try {
+      const {data} = await axios.get(backendUrl + '/api/auth/is-auth')
+      if (data.success) {
+        setIsLoggedin(true)
+        getUserData()
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const getUserData = async () => {
+    try {
+      const { data: responseData } = await axios.get(backendUrl + '/api/user/data');
+      console.log(responseData);
+      responseData.success
+        ? setUserData(responseData.userData || {})
+        : toast.error(responseData.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(()=>{
+    getAuthState()
+  }, [])
+
+  const value = {
+    backendUrl,
+    isLoggedin,
+    setIsLoggedin,
+    userData,
+    setUserData,
+    getUserData
+  };
+
+  return (
+    <AppContent.Provider value={value}>
+      {children}
+    </AppContent.Provider>
+  );
+};
