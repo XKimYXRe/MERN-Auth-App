bed5ac988f5b2d3a2a5225ef67ca0a5c40679ee1
OXKIMYXO <dexmareborn0101@gmail.com>
2025-04-30 11:16:46 +0200

    Set cookie secure and sameSite attributes to 'none' for consistent behavior

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -28,8 +28,8 @@ export const register = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
@@ -77,8 +77,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
